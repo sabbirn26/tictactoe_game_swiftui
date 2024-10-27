b8db3ac38a535a03be5b3944b02eabed6dd7d0d0
Sabbir <nasir@newroztech.com>
2024-10-27 15:16:18 +0600

    initial commit

---
+++ XOXO-TicTacToe/XOXO-TicTacToe/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  XOXO-TicTacToe
+//
+//  Created by Sabbir Nasir on 27/10/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ XOXO-TicTacToe/XOXO-TicTacToe/XOXO_TicTacToeApp.swift
@@ -0,0 +1,17 @@
+//
+//  XOXO_TicTacToeApp.swift
+//  XOXO-TicTacToe
+//
+//  Created by Sabbir Nasir on 27/10/24.
+//
+
+import SwiftUI
+
+@main
+struct XOXO_TicTacToeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
