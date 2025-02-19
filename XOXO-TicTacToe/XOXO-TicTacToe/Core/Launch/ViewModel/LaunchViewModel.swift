166297a0b1ef48f4e976f0017e367a19dadb66a2
Sabbir <62333547+sabbirn26@users.noreply.github.com>
2025-02-19 17:14:21 +0600

    - refactored: Launch view model

--- XOXO-TicTacToe/XOXO-TicTacToe/Core/Launch/ViewModel/LaunchViewModel.swift
+++ XOXO-TicTacToe/XOXO-TicTacToe/Core/Launch/ViewModel/LaunchViewModel.swift
@@ -14,28 +14,30 @@ class LaunchViewModel: ObservableObject {
     @Published var counter: Int = 0
     @Published var loops: Int = 0
     private var timer: Timer?
+    @Binding var showLaunchView: Bool
     
-    init() {
+    init(showLaunchView: Binding<Bool>) {
+        self._showLaunchView = showLaunchView
         self.loadingText = "Whoever loses is kinda gay...".map { String($0) }
     }
     
-    func startTimer(completion: @escaping () -> Void) {
+    func startTimer() {
         showLoadingText = true
-        timer = Timer.scheduledTimer(withTimeInterval: 0.1, repeats: true) { [weak self] _ in
-            self?.loopCounterMethod(completion: completion)
+        timer = Timer.scheduledTimer(withTimeInterval: 0.1, repeats: true) { _ in
+            self.loopCounterMethod()
         }
     }
     
-    private func loopCounterMethod(completion: @escaping () -> Void) {
-        withAnimation(.easeInOut(duration: 0.1)) {
+    private func loopCounterMethod() {
+        withAnimation(.spring()) {
             let lastIndex = loadingText.count - 1
             if counter == lastIndex {
                 counter = 0
                 loops += 1
                 
                 if loops >= 1 {
+                    showLaunchView = false
                     timer?.invalidate()
-                    completion()  // Call completion handler to change `showLaunchView`
                 }
             } else {
                 counter += 1
@@ -43,4 +45,3 @@ class LaunchViewModel: ObservableObject {
         }
     }
 }
-
