66aa19876254ed143a2b3610239c80eb3c669d80
Sabbir <62333547+sabbirn26@users.noreply.github.com>
2025-02-19 17:03:29 +0600

    - refactored: HapticManager

--- XOXO-TicTacToe/XOXO-TicTacToe/Utilities /HapticManager.swift
+++ XOXO-TicTacToe/XOXO-TicTacToe/Utilities /HapticManager.swift
@@ -8,10 +8,15 @@
 import Foundation
 import SwiftUI
 
-class HapticManager{
-  static private let generator = UINotificationFeedbackGenerator()
-   
-  static func notification(type: UINotificationFeedbackGenerator.FeedbackType){
-    generator.notificationOccurred(type)
-  }
+class HapticManager {
+    // A private haptic feedback generator instance (because we don't want everyone messing with it 😉)
+    static private let generator = UINotificationFeedbackGenerator()
+    
+    /// Triggers a haptic notification feedback.
+    /// - Parameter type: The type of haptic feedback (success, warning, error).
+    static func notification(type: UINotificationFeedbackGenerator.FeedbackType) {
+        print("🔔 Haptic feedback triggered: \(type)")
+        generator.notificationOccurred(type)
+    }
 }
+
