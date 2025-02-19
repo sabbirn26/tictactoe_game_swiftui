ff4b1012c4133801242a0d6a872c9b97954aea25
Sabbir <62333547+sabbirn26@users.noreply.github.com>
2025-02-19 17:07:32 +0600

    - refactored: reusable circle button view

--- XOXO-TicTacToe/XOXO-TicTacToe/Core/Components/CircleButtonView.swift
+++ XOXO-TicTacToe/XOXO-TicTacToe/Core/Components/CircleButtonView.swift
@@ -8,18 +8,19 @@
 import SwiftUI
 
 struct CircleButtonView: View {
-    let iconName : String
+    let iconName: String
+    
     var body: some View {
         Image(systemName: iconName)
             .font(.headline)
             .foregroundColor(.white)
-            .frame(width:40, height: 40)
-            .background{
+            .frame(width: 40, height: 40)
+            .background {
                 Circle()
-                    .foregroundColor(.blue.opacity(0.5))
-                    .shadow(radius: 20, y: 10)
+                    .foregroundColor(.blue.opacity(0.5)) // Soft blue background for a sleek look
+                    .shadow(radius: 20, y: 10) // Adds a cool floating effect
             }
-            .shadow(color: .black,radius: 10)
+            .shadow(color: .black, radius: 10) // Extra depth for aesthetics
             .padding()
     }
 }
