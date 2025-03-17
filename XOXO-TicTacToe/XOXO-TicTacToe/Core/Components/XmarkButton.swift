c97a7fb5d50405167534c4f5ef9d7e4261aff7c9
Sabbir <62333547+sabbirn26@users.noreply.github.com>
2025-03-17 14:02:18 +0600

    -refactored: XmarkButton

--- XOXO-TicTacToe/XOXO-TicTacToe/Core/Components/XmarkButton.swift
+++ XOXO-TicTacToe/XOXO-TicTacToe/Core/Components/XmarkButton.swift
@@ -7,23 +7,30 @@
 
 import SwiftUI
 
+/// A custom button view that displays an "X" (close) icon.
+/// This button is typically used to dismiss a bottom sheet or modal.
 struct XmarkButton: View {
+    /// Closure to handle the dismissal action.
     let dismiss: () -> Void
     
     var body: some View {
         Button(action: {
+            // Print a message to the console when the button is tapped.
             print("❌ Portfolio bottom sheet dismissed!")
+            // Call the dismiss closure to perform the actual dismissal.
             dismiss()
         }, label: {
-            Image(systemName: "xmark")
-                .font(.headline)
-                .fontWeight(.heavy)
-                .foregroundStyle(Color.white)
+            Image(systemName: "xmark") // Use the "xmark" system image.
+                .font(.headline) // Set the font size to headline.
+                .fontWeight(.heavy) // Make the font weight heavy for better visibility.
+                .foregroundStyle(Color.white) // Set the icon color to white.
         })
     }
 }
 
+/// A preview of the `XmarkButton` for SwiftUI's canvas.
 #Preview {
     XmarkButton(dismiss: {})
 }
+
 
